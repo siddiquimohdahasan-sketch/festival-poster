a5e11e42ed844f3395ea482974667d7c62624b86
siddiquimohdahasan-sketch <siddiquimohdahasan@gmail.com>
2026-02-14 16:05:19 +0530

    Update server.js health set

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("/health", (req, res) => res.send("OK"));
+
 
 // ✅ Multer memory storage (photo upload)
 const upload = multer({ storage: multer.memoryStorage() });
