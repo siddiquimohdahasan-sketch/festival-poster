ec0df63cc2effcbaadbff908f5c22e5b94f1afef
Mohd Ahasan Siddiqui <siddiquimohdahasan@gmail.com>
2026-02-14 18:02:25 +0530

    fix health check

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // ✅ Root route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  app.get("/", (req, res) => {
+  res.send("Server Running");
 });
+
 app.get("/health", (req, res) => res.send("OK"));
 
 
